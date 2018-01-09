3718ea132448e116ab577d364e124a72fff259ea
cbrusch <crystal_brusch@brown.edu>
2018-01-09 17:04:22 -0500

    I can now pull text from all emblem sections

--- app/js/translation.js
+++ app/js/translation.js
@@ -7,13 +7,19 @@ $(document).ready(function() {
 	var emblemDataNum = $('.wrapper-emblem').data("id"); // get a numerical ID for the current emblem page
 	while (i < 50+1) { // make an array of urls to each english emblem proof page
 		if (i < 10){ // add a leading zero to the emblem number in the url if the emblem is less than 10
-			englishEmblemArray.push("partials/emblems-html/emblem0" + i++ + "-Facsimile.html #motto-e");
+			englishEmblemArray.push("partials/emblems-html/emblem0" + i++ + "-Facsimile.html #mottoPartial--e");
 		}
 		else {
-			englishEmblemArray.push("partials/emblems-html/emblem" + i++ + "-Facsimile.html #motto-e");
+			englishEmblemArray.push("partials/emblems-html/emblem" + i++ + "-Facsimile.html #mottoPartial--e");
 		}
 	}
 	englishEmblemURL = englishEmblemArray[emblemDataNum]; // associate the URL with the corresponding emblem data ID
 	$('._motto--english').load(englishEmblemURL); // load the emblem text from the corresponding URL
+	$('._epigram--english').load('partials/emblems-html/emblem01-Facsimile.html #epigramPartial--e');
+	$('._discourse--english').load('partials/emblems-html/emblem01-Facsimile.html #discoursePartial--e');
 	console.log(englishEmblemArray[emblemDataNum]);
+
+	function loadEmblem() {
+		$('._motto--english').load(englishEmblemURL); // load the emblem text from the corresponding URL
+	}
 })
