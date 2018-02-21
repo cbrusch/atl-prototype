5524a6453737558f7a30ebea83d74caac727f683
cbrusch <crystal_brusch@brown.edu>
2018-02-21 17:23:59 -0500

    including content from front matter

--- app/js/translation.js
+++ app/js/translation.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-	console.log("the translation.js file loaded");
 	var topNavEl = "._topnav";
 	var topNavPartial = "../partials/_topnav.html";
-	var i = 1;
+	var i
+	var frontispieceElEnglish = "._frontispiece--english";
+	var bioElEnglish = "._bio--english";
+	var prefaceElEnglish = "._preface--english";
 	var mottoElEnglish = "._motto--english";
 	var epigramElEnglish = "._epigram--english";
 	var discourseElEnglish = "._discourse--english";
@@ -23,6 +25,10 @@ $(document).ready(function() {
 	var facsimilePartial = "../partials/_facsimile.html";
 	var facsimileHalfEl = "._facsimile-half";
 	var facsimileHalfPartial = "../partials/_facsimileHalf.html";
+	var frontMatterXML = "../partials/emblems-html/Introduction-Read.html";
+	var frontispiecePartial = frontMatterXML + " #frontispiecePartial--e";
+	var bioPartial = frontMatterXML + " #bioPartial--e";
+	var prefacePartial = frontMatterXML + " #prefacePartial--e";
 
 
 	generateJSON();
@@ -36,16 +42,16 @@ $(document).ready(function() {
 		for (i = 1; i < 51; i++) { // make an array of emblem html files with values of urls + IDs to the content sections
 			if (i < 10) { // add a leading zero to the emblem number in the url if the emblem is less than 10
 				englishEmblemArray.push({
-					motto: "../../partials/emblems-html/emblem0" + i + "-Facsimile.html #mottoPartial--e h3.title",
-					epigram: "../partials/emblems-html/emblem0" + i + "-Facsimile.html #epigramPartial--e .verse-epigram",
-					discourse: "../partials/emblems-html/emblem0" + i + "-Facsimile.html #discoursePartial--e"
+					motto: "../partials/emblems-html/emblem0" + i + "-Read.html #mottoPartial--e h3.title",
+					epigram: "../partials/emblems-html/emblem0" + i + "-Read.html #epigramPartial--e .verse-epigram",
+					discourse: "../partials/emblems-html/emblem0" + i + "-Read.html #discoursePartial--e"
 				});	
 			}
 			else {
 				englishEmblemArray.push({
-					motto: "../partials/emblems-html/emblem" + i + "-Facsimile.html #mottoPartial--e h3.title",
-					epigram: "../partials/emblems-html/emblem" + i + "-Facsimile.html #epigramPartial--e .verse-epigram",
-					discourse: "../partials/emblems-html/emblem" + i + "-Facsimile.html #discoursePartial--e"
+					motto: "../partials/emblems-html/emblem" + i + "-Read.html #mottoPartial--e h3.title",
+					epigram: "../partials/emblems-html/emblem" + i + "-Read.html #epigramPartial--e .verse-epigram",
+					discourse: "../partials/emblems-html/emblem" + i + "-Read.html #discoursePartial--e"
 				});	
 			}
 		}
@@ -59,13 +65,15 @@ $(document).ready(function() {
 
 	function loadPartials(){
 		$(topNavEl).load(topNavPartial); // load the global navigation partial
-		$(mottoElEnglish).load(englishEmblemURL["motto"]); // load the motto text from the corresponding file
-		$(epigramElEnglish).load(englishEmblemURL["epigram"]); // load the epigram text from the corresponding file
-		$(discourseElEnglish).load(englishEmblemURL["discourse"]); // load the discourse text from the corresponding file
+		$(frontispieceElEnglish).load(frontispiecePartial); // load the frontispiece text from the XML file
+		$(bioElEnglish).load(bioPartial); // load the bio text from the XML file
+		$(prefaceElEnglish).load(prefacePartial); // load the preface text from the XML file
+		$(mottoElEnglish).load(englishEmblemURL["motto"]); // load the motto text from the XML file
+		$(epigramElEnglish).load(englishEmblemURL["epigram"]); // load the epigram text from the XML file
+		$(discourseElEnglish).load(englishEmblemURL["discourse"]); // load the discourse text from the XML file
 		$(sideNavEl).load(sideNavPartial); // load the side nav partial
 		$(imageEl).load(imagePartial); // load the emblem image partial
 		$(facsimileEl).load(facsimilePartial); // load the full facsimile image viewer partial
 		$(facsimileHalfEl).load(facsimileHalfPartial); // load the half facsimile image viewer partial
 	}
-
 })
